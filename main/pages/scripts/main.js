beff1f493ada9cace1075db36be7fa2f29fd9746
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2026-05-10 23:17:21 +1000

    Item Assets + Initial Files
    I have completed all of the initial item assets  and the front view of my character + music has also been added. I have also taken previous work of mine to begin the creation of the accounts page. No testing of the game/sigin logic as unfinished. Next I will continue the game logic, assets and login process for the game.

---
+++ main/pages/scripts/main.js
@@ -0,0 +1,49 @@
+// Initialisation 
+score_ = 0 
+
+// Classes
+class Character{
+    constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class item{
+    constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class life_item extends item{
+    constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class Luck_item extends item{
+    constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class Mobility_item extends item{
+    constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+// Functions
+function start_game(){
+    
+}
+
+
+function end_screen(){
+    
+}
+
+function end_game(){
+
+}
+
+// Game Loop
---
+++ main/pages/scripts/signup.js
@@ -0,0 +1,70 @@
+function password_test(message){
+    // Fetching the length og the password for testing 
+    const password_len_test = message.length 
+    // Here we use regex's to test for numbers and letters
+    const has_number = /[0-9]/.test(message);
+    const has_letter = /[a-zA-Z]/.test(message);
+    // Debugging statement that logs the result of the password test to the console (true or false)
+    console.log(has_number && has_letter && password_len_test >= 8)
+    // The operand && will return true only if both values are true
+    return has_number && has_letter && password_len_test >= 8;
+}
+
+function email_test(email){
+    // This operand functions by testing a regex (more complicated version of above)
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    // It ensures the format of the email follows this : "____@__.__"
+}
+
+function check(){
+    // This firstly checks that the email contains a .com and an @ symbol
+    let name_ = document.getElementById("name_").value
+    let dob = document.getElementById("DOB").value
+    let password = document.getElementById("password").value
+    let email = document.getElementById("email").value
+    console.log(email_test(email))
+    // It also checks that the password includes by using the exisisting password testing function
+    // The operand && will return true only if all values are true
+    if (email_test(email) === true && password_test(password) === true){
+        // Debugging statement
+        console.log("All valid values")
+        alert("Redirecting to results page")
+        // Saves the infomation to local storage to display back to user on results page
+        localStorage.setItem("Name",name_)
+        localStorage.setItem("Email",email)
+        localStorage.setItem("Password",password)
+        localStorage.setItem("DOB",dob)
+        // You redirect here
+        window.location.href = "results.html"
+    }else{
+        // Alerts the user
+        alert("A problem has occured, please ensure all values are valid.")
+    }
+}
+
+function show_password_toggle(){
+    // Gets the password element
+    let password_ = document.getElementById("password")
+    // Checks the current type of the element (checks if its in hidden form or text form)
+    // It then changes to opposite using a switch if statement
+    if (password_.type === "password"){
+        password_.type = "text"
+    }else{
+        password_.type = "password"
+    }
+}
+
+function password_info_check(){
+    // Gets the password element and the informative text above
+    let password_2 = document.getElementById("password").value
+    let password_info = document.getElementById("password_info")
+    // Uses the password test function to check if the password is fufilling requirements or not in an if statement
+    // Responds by changing the innfomative text's colour and text content
+    if (password_test(password_2) === false){
+        password_info.style.color = "red";
+        password_info.textContent  = "Password must contain letters, numbers and at least 8 character long."
+    }else{
+        password_info.style.color = "black";
+        password_info.textContent  = "Password:" 
+    }
+}
