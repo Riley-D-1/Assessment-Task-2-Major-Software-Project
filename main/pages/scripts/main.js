8fa958d859daa27a1f2f309b154fc10ca5fadc42
Riley-D-1 <riley.dowse@education.nsw.gov.au>
2026-05-25 11:09:41 +1000

    Worked on trail system and game logic
    Began work upon the trail system for my game and the angle system for character and other information. Next I will complete the database and login system before working on the character's controls which are crucial for success.

--- main/pages/scripts/main.js
+++ main/pages/scripts/main.js
@@ -1,5 +1,10 @@
 // Initialisation 
 score_ = 0 
+starting_pos = [0,0]
+character_angle = 0 
+previous_coords_array = []
+for(let i =0; i> 50 ;i++)
+	previous_coords_array.unshift(starting_pos)
 
 // Classes
 class Character{
@@ -33,12 +38,49 @@ class Mobility_item extends item{
   }
 }
 // Functions
+function menu(){
+
+}
 function start_game(){
-    
+
 }
 
-function ski_calculate(){
+function tile_generator(){
+	random_generator = Math.floor(Math.random() * 10);
+	if (random_generator <= 0){
 
+	}else if (random_generator <= 1){
+		
+	}
+}
+
+
+function player_sprite(){
+	if (player_angle == 90){
+		return "main/assets/character/front.png"
+	}else if (player_angle == 90){
+		return "main/assets/character/front.png"
+	}else if (player_angle == 90){
+		return "main/assets/character/front.png"
+	}else{
+		return "main/assets/character/front.png"
+	}
+}
+function ski_calculate(){
+	if (player_angle){
+		let ski_seperation = 0.1
+	}
+}
+
+function character_draw(x,y,current_sprite_dir){
+	ctx.fillRect(25, 25, x, y);
+	/*
+	const image = ;
+	image.addEventListener("load", (e) => {
+	ctx.drawImage(image, 10, 10);
+	});
+	*/
+	
 }
 
 function ski_draw(){
@@ -49,19 +91,37 @@ function end_screen(){
     
 }
 
+function trail(previous_coords_array,new_coords){
+   	let trail_radius = 1
+	let coords_array = previous_coords_array
+	coords_array.unshift(new_coords)
+	coords_array.pop()
+	for (let i = 0; i < length(coords_array); i++) {
+		ctx.beginPath();
+		let seperation = 0.1
+ 		let transperncy = 1/i 
+		ctx.arc((current_array_x - seperation), current_array_y, trail_radius, 0, 2 * Math.PI);
+		ctx.fill();
+		// Second Circle
+		ctx.beginPath();
+		ctx.arc((current_array_x + seperation), current_array_y, trail_radius, 0, 2 * Math.PI);
+		ctx.fillStyle = `rgb(198 236 249 / ${transperncy})`;
+		ctx.fill();
+}
+}
 function end_game(){
 
 }
 function play_music(stage){
 	if (stage === "game"){
-    	const audio = new Audio('../../music/assets.ogg')
+    	let audio = new Audio('../../music/assets.ogg')
 	}else{
-    	const audio = new Audio('../../music/assets.ogg')
+		let audio = new Audio('../../music/assets.ogg')
 	}
     audio.play()
 }
 
 // Game Loop
 function main(){
-
+	
 }
